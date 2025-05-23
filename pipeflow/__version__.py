7c970526a2f787be631f3a94f8cc285a23871844
James Neill <git@jamesneill.codes>
2025-05-23 22:46:32 +0200

    fix(version): default to `dev` if package is not installed

--- pipeflow/__version__.py
+++ pipeflow/__version__.py
@@ -3,4 +3,7 @@ from __future__ import annotations
 from importlib import metadata
 
 APP_NAME = "pipeflow"
-__version__ = metadata.version(APP_NAME)
+try:
+    __version__ = metadata.version(APP_NAME)
+except metadata.PackageNotFoundError:
+    __version__ = "dev"
