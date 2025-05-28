3827a3659b998ab649334842e62759dca9e5e83a
James Neill <git@jamesneill.codes>
2025-05-28 21:21:37 +0200

    chore: remove unused functions

--- flowfunc/utils/helpers.py
+++ flowfunc/utils/helpers.py
@@ -20,33 +20,6 @@ def directory(path: Path):
     finally:
         os.chdir(cwd)
 
-
-def merge_dicts(d1: dict[str, Any], d2: dict[str, Any]) -> None:
-    for k in d2:
-        if k in d1 and isinstance(d1[k], dict) and isinstance(d2[k], Mapping):
-            merge_dicts(d1[k], d2[k])
-        else:
-            d1[k] = d2[k]
-
-
-def ensure_path(path: str | Path, is_directory: bool = False) -> Path:
-    if isinstance(path, str):
-        path = Path(path)
-
-    if path.exists() and path.is_dir() == is_directory:
-        return path
-
-    raise ValueError(
-        f"Specified path '{path}' is not a valid {'directory' if is_directory else 'file'}."
-    )
-
-
-def pluralize(count: int, word: str = "") -> str:
-    if count == 1:
-        return word
-    return word + "s"
-
-
 def sanitize_string(data: str) -> str:
     sanitized = "".join(c if c.isalnum() or c in ["-", "_"] else "_" for c in data)
     remove_duplicate_underscores = re.sub(r"_+", "_", sanitized)
